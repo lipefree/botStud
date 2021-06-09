d688bc6e0831b71c8b8335cf75e65f3d55873365
qngo <quang.ngo@epfl.ch>
2021-06-09 23:04:37 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord as disc
+
+client = disc.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        print("Message received")
+        await message.channel.send('Hello!')
+
+    print('Message from {0.author}: {0.content}'.format(message))
+
+
+
+client.run('ODUyMjg3OTU4MDA2NzU5NDU0.YMEpAw.ubIT1qL5RGPFhf1PpDGd8bsBp6k')
