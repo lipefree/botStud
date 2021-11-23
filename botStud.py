d51ea1d0fb24637cc8f8c9065ed4d14238472aa7
lipefree <quang.ngo@epfl.ch>
2021-11-23 14:49:32 +0100

    refactor

--- botStud.py
+++ botStud.py
@@ -5,34 +5,53 @@ from discord.ext import commands, tasks
 import youtube_dl
 import os
 import asyncio
-import DiscordToken 
+import DiscordToken
+from cogs.StudyRoom import Music
 
-intents = discord.Intents().default()
-intents.voice_states = True
-client = commands.Bot(command_prefix="!",intents=intents)
+# intents = discord.Intents().default()
+# intents.voice_states = True
+# client = commands.Bot(command_prefix="!",intents=intents)
 
 #cogs organisation
 
-initital_extensions = ['cogs.studyRoom']
+# initital_extensions = ['cogs.studyRoom']
 
 #cogs loading
 
-if __name__ == '__main__':
-    for extension in initital_extensions :
-        client.load_extension(extension)
+# if __name__ == '__main__':
+#     for extension in initital_extensions :
+#         client.load_extension(extension)
 
-@client.event
+# @client.event
+# async def on_ready():
+#     print('We have logged in as {0.user}'.format(client))
+#     checkIfAlone.start()
+
+# @tasks.loop(seconds = 10)
+# async def checkIfAlone():
+#     if len(client.voice_clients) != 0 :
+#         if len(client.voice_clients[0].channel.members) == 1 :
+#             await client.voice_clients[0].disconnect()
+
+# print("working")
+
+# @commands.command(name='version')
+#     async def version(self, ctx):
+#         message = f"[{datetime.datetime.now()}] version by {ctx.author.name}\n"
+#         message += subprocess.run(["git", "log", "-1"], cwd="/home/llama/botStud", capture_output=True).stdout.decode('unicode_escape')
+#         print(message)
+#         await ctx.send(message)
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+                   description='Relatively simple music bot example')
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    checkIfAlone.start()
+    print('Logged in as {0} ({0.id})'.format(bot.user))
+    print('------')
 
-@tasks.loop(seconds = 10)
-async def checkIfAlone():
-    if len(client.voice_clients) != 0 :
-        if len(client.voice_clients[0].channel.members) == 1 :
-            await client.voice_clients[0].disconnect()
+bot.add_cog(Music(bot))
+bot.run(DiscordToken.TOKEN)
 
-print("working")
-
-client.run(DiscordToken.TOKEN)
+# client.run(DiscordToken.TOKEN)
 
